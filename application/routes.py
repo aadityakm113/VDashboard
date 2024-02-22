d61c2f78692067a638a4251ad984eaf99ab5fe49
aadityakm113 <aadityakm113@gmail.com>
2024-02-22 10:26:40 +0530

    display-data/routing

--- application/routes.py
+++ application/routes.py
@@ -4,14 +4,10 @@ from bson import json_util
 import json
 
 
-@app.route('/')
+@app.route('/list')
 def lists():
     data = db.data.find_one({"end_year": 2022})
-    
-    # Convert BSON document to JSON
     json_data = json_util.dumps(data)
-    
-    # Return JSON response
     return json.loads(json_data)
     #return json.loads(json_util.dumps(db.data.find_one({"end_year": 2022})))
-
+    
